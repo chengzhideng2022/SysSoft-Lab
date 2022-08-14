578a05fd09abcf68f663a3aee36373b2393bb6f4
chengzhi <chengzhi.deng@student.kuleuven.be>
2022-08-14 19:44:01 +0200

    ex1

--- ex1/main.c
+++ ex1/main.c
@@ -1,14 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <assert.h>
+int main() {
+	//	printf("Hello world\n");
+	//	system("clear");
+	//	system("ls -l > files.txt");
+	//	system("sort files.txt | more");
 
-int main(void)
-
-{
-
-printf("Hello, world!");
-
-return 0;
-
+	int sizeof_int = sizeof(int);
+	printf("sizeof(int) = %d\n", sizeof_int);
+	int sizeof_char = sizeof(char);
+	printf("sizeof(char) = %d\n", sizeof_char);
+	int sizeof_short = sizeof(short);
+	printf("sizeof(short) = %d\n", sizeof_short);
+	int sizeof_float = sizeof(float);
+	printf("sizeof(float) = %d\n", sizeof_float);
+	int sizeof_long = sizeof(long);
+	printf("sizeof(long) = %d\n", sizeof_long);
+	int sizeof_long_long = sizeof(long long);
+	printf("sizeof(long long) = %d\n", sizeof_long_long);
+	int sizeof_double = sizeof(double);
+	printf("sizeof(double) = %d\n", sizeof_double);
+	int sizeof_void = sizeof(void);
+	printf("sizeof(void) = %d\n", sizeof_void);
+	int sizeof_void_ptr = sizeof(void*);
+	printf("sizeof(void*) = %d\n", sizeof_void_ptr);
+	return 0;
 }
-
