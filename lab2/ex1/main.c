ffe75b050a821c55a33014513eb1f97650f8a4e9
chengzhi <chengzhi.deng@student.kuleuven.be>
2023-01-03 02:47:00 +0100

    X X
    :WQ
    :wq
    wq
    
    WQ!
    Q!
    ZXB#	lab2/ex1/main
    wq
    wq
    q
    :wq
    
    hl;lnh;lkZZ
    X
    
    O
    TXT

--- lab2/ex1/main.c
+++ lab2/ex1/main.c
@@ -1,26 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <unistd.h>
 
 #define FREQUENCY                       5
-#define MINIMUM_TEMPERATURE ( -10 * 100 )
-#define MAXIMUM_TEMPERATURE (  35 * 100 )
+#define MINIMUM_TEMPERATURE -10
+#define MAXIMUM_TEMPERATURE 35
 
-void read_sensor ( void ) {
-    float random_temperature = ( ( MINIMUM_TEMPERATURE + rand ( ) % ( MAXIMUM_TEMPERATURE - MINIMUM_TEMPERATURE ) ) / 100.0 );
-    printf ( "Temperature = %05.2f @\t",random_temperature );
-    fflush ( stdout );
-    system ( "date" );
-}
+int main() {
+    srand(time(NULL));  
 
-int main ( int argc, char * argv [ ] ) {
-    srand ( time ( NULL ) );
-
-    while ( 1 ) {
-        read_sensor ( );
-        sleep ( FREQUENCY );
+    while (1) {
+        float num = (float)(rand()% (MAXIMUM_TEMPERATURE*100 - MAXIMUM_TEMPERATURE*100+100) + MINIMUM_TEMPERATURE*100 ) / 100; 
+        printf("Temperature = %1.2f @", num);
+        fflush ( stdout );
+        
+        system("date");
+       
+        sleep(5); 
     }
-
-    return ( 0 );
+    return 0;
 }
